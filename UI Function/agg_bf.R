30a4e93ab75a0348898f98f10b9d898feb831bc2
Eli-Boaz Clapper <74557493+EliClapper@users.noreply.github.com>
2022-04-26 19:05:13 +0200

    Add varying constraints
    - Equal sign constraints does not seem to work
    - Direction of hypothesized value not yet implemented
    - Check if multivariate case must implemented

--- UI Function/agg_bf.R
+++ UI Function/agg_bf.R
@@ -1,28 +1,24 @@
-# this functions main input must be a formula, for example y ~ x as well as a hypothesis, for example x > 0.2.
-# the user must also specify the variable that represents the groups
-# this function than tests the hypothesis for multiple groups and calculates an aggregated BF.
-# the user can specify which algorithm to use (for example, "allsig", "gpbf_ic", "prodbf_ic", "prodbf_iu" and "tbf_ic").
+# agg_bf() is a function that can aggregated Bayes Factors for hypotheses.
+# For now, only correlation coefficients are supported, but this could be expanded to formulas and multiple hypotheses.
+# Suppose the hypothesis is that the correlation coefficient of y and x > 0.2.
+# Then for every k, the BF is calculated for the formula y ~ x based on the algorithm the user selects (for example, "allsig", "gpbf_ic", "prodbf_ic", "prodbf_iu" and "tbf_ic").
+# Finally it is aggregated using some algorithm specified and this aggregated BF is returned.
+# This means the user must also specify the variable that represents the groups.
 
 
-# for example, an input matrix can look as follows
-set.seed(6164900)
-y <- rnorm(1000)
-x <- y + rnorm(1000,0,3)
-cor(x,y)
-M <- cbind(y = y,            # generate outcome variable from std normal dist
-           x = x,        # generate predictor from normal dist
-           k = rep(c("A", "B", "C", "D"), each = 250))   # make 4 groups
-# View(M)
+# The function below is an example of what it would look like.
+# It relies on four other functions.
+# 1.) preprocess_data(): function to make data ready to be used by bain
+# 2.) allsig(): function to calculate allsig BF (do not know this algorithm, however and might be incorrect)
+# 3.) tbf(): calculates tbf_ic and tbf_iu
+# 4.) prod_and_gpbf: calculated prodbf_ic, prodbf_iu, gpbf_ic, gpbf_iu
 
-# suppose the hypothesis is that the correlation coefficient of x and y > 0.2
-# then for every k, the BF is calculated for the formula y ~ x 
-# finally it is aggregated using some algorithm specified and this aggregated BF is returned
-
-# this function below is an example of what it would look like.
-# it relies on three other functions that occur in the simulation
+# for example, an input data frame can look as follows, where z remains unused for now
 source(file.path("UI Function", "support_functions.R"))
+M <- example_df()
 
-agg_bf <- function(formula = F, grouping, data, algorithm, hypothesis){
+# the function
+agg_bf <- function(formula, grouping, data, algorithm, hypothesis){
   
   # see if bain is can be loaded
   tryCatch(library(bain), error = function(e){stop("installation of package 'bain' is required")})
@@ -34,42 +30,37 @@ agg_bf <- function(formula = F, grouping, data, algorithm, hypothesis){
                paste(supported_algorithms, collapse = ", ")))
   }
   
-  # convert to data frame for easy subsetting and obtain vector unique groups
-  data <- as.data.frame(data)                    
-  unique_groups <- unique(data[grouping])[,1]    
-  k <- length(unique_groups)
-  if(!k > 1){stop("At least two groups are required to aggregate Bayes Factors")}
+  # convert data to data.frame and subset for only variables used in analysis
+  data <- tryCatch(as.data.frame(data), error = function(e){stop('Make sure data can be converted to data.frame')})
+  data <- cbind(stats::model.frame(formula, data), data[grouping])
+  if(ncol(data) < 3){stop("data must have at least 3 columns")}
   
-  # obtain the hypothesized value
-  hyp_val <- as.numeric(unlist(regmatches(hypothesis, gregexpr("[[:digit:]]+\\.*[[:digit:]]*",hypothesis))))
-  
-  # obtain per group the data.frame to calculate BF on.
-  df_groups <- lapply(unique_groups, function(group){
-    df_group <- subset(data[data[grouping] == group, ], select = -eval(parse(text = grouping)))
-    sapply(df_group, as.numeric)
-  })
-  
-  # get the correlation coefficient and standard error per group 
-  res <- sapply(df_groups, function(df_group){
-    est <- cor(df_group)[2,1]                        #estimate for correlation between x and y
-    se_est <- sqrt((1 - est^2)/(nrow(df_group) - 2)) #estimate for standard error = sqrt((1-r^2)/df) with df = N - 2
-    c(est, se_est)               #return estimate, se and sample size of particular set
-  })
-  colnames(res) <- paste0('r', 1:k)
+  # prepare data for bain and obtain parameters to be passed to tbf() and prod_and_gpbf() functions
+  prep <- preprocess_data(data, hypothesis, grouping)
   
   # return bf based on chosen algorithm
   out <- switch(algorithm,
-      allsig ={allsig(res, hyp_val)},
-      tbf_ic = {tbf(res, hyp_val, floor(nrow(data)/k), k)['tbf_ic']},
-      tbf_iu = {tbf(res, hyp_val, floor(nrow(data)/k), k)['tbf_iu']},
-      prodbf_ic ={prod_and_gpbf(res, hyp_val, floor(nrow(data)/k), k)['prodbf_ic']},
-      prodbf_iu ={prod_and_gpbf(res, hyp_val, floor(nrow(data)/k), k)['prodbf_iu']},
-      gpbf_ic ={prod_and_gpbf(res, hyp_val, floor(nrow(data)/k), k)['gpbf_ic']},
-      gpbf_iu ={prod_and_gpbf(res, hyp_val, floor(nrow(data)/k), k)['gpbf_iu']})
+      allsig    = {allsig(prep$res, prep$hyp_val)},
+      tbf_ic    = {tbf(prep$res, prep$hyp_val, floor(nrow(data)/prep$k), prep$k, prep$constraint)['tbf_ic']},
+      tbf_iu    = {tbf(prep$res, prep$hyp_val, floor(nrow(data)/prep$k), prep$k, prep$constraint)['tbf_iu']},
+      prodbf_ic = {prod_and_gpbf(prep$res, prep$hyp_val, floor(nrow(data)/prep$k), prep$k, prep$constraint)['prodbf_ic']},
+      prodbf_iu = {prod_and_gpbf(prep$res, prep$hyp_val, floor(nrow(data)/prep$k), prep$k, prep$constrain)['prodbf_iu']},
+      gpbf_ic   = {prod_and_gpbf(prep$res, prep$hyp_val, floor(nrow(data)/prep$k), prep$k, prep$constrain)['gpbf_ic']},
+      gpbf_iu   = {prod_and_gpbf(prep$res, prep$hyp_val, floor(nrow(data)/prep$k), prep$k, prep$constrain)['gpbf_iu']})
   
   return(out)
 }
 
-agg_bf(F, grouping = 'k', data = M, algorithm = 'prodbf_ic', hypothesis = 'x>0.2')
-agg_bf(F, grouping = 'k', data = M, algorithm = 'tbf_ic', hypothesis = 'x>0.2')
+# test for five algorithms and three different constraints
+(c(true_effect = cor(M$x, M$y)))
+agg_bf(y~x, grouping = 'k', data = M, algorithm = 'prodbf_ic', hypothesis = '>0.2')
+agg_bf(y~x, grouping = 'k', data = M, algorithm = 'tbf_ic', hypothesis = '>0.2')
+agg_bf(y~x, grouping = 'k', data = M, algorithm = 'gpbf_ic', hypothesis = '<0.2')
+
+#the 'equals' constraint seems to malfunction
+agg_bf(y~x, grouping = 'k', data = M, algorithm = 'gpbf_iu', hypothesis = '= 0') 
+
+#also does not seem to work yet for negative hypothesized values
+agg_bf(y~x, grouping = 'k', data = M, algorithm = 'prodbf_iu', hypothesis = '<-0.5')
+
 
--- UI Function/support_functions.R
+++ UI Function/support_functions.R
@@ -1,5 +1,35 @@
 # Support functions
 
+##### Prepares data for bain() -------------------------------------------------
+preprocess_data <- function(data, hypothesis, grouping){
+  # obtain vector of unique groups
+  unique_groups <- unique(data[grouping])[,1]    
+  k <- length(unique_groups)
+  if(!k > 1){stop("At least two groups are required to aggregate Bayes Factors")}
+  
+  # obtain the hypothesized value
+  hyp_val <- as.numeric(unlist(regmatches(hypothesis, gregexpr("[[:digit:]]+\\.*[[:digit:]]*",hypothesis))))
+  constraint <- unlist(regmatches(hypothesis, gregexpr("(>|<|=)", hypothesis)))
+  
+  # obtain per group the data.frame to calculate BF on.
+  df_groups <- lapply(unique_groups, function(group){
+    df_group <- subset(data[data[grouping] == group, ], select = -eval(parse(text = grouping)))
+    sapply(df_group, as.numeric)
+  })
+  
+  # get the correlation coefficient and standard error per group 
+  res <- sapply(df_groups, function(df_group){
+    est <- cor(df_group)[2,1]                        #estimate for correlation between x and y
+    se_est <- sqrt((1 - est^2)/(nrow(df_group) - 2)) #estimate for standard error = sqrt((1-r^2)/df) with df = N - 2
+    c(est, se_est)               #return estimate, se and sample size of particular set
+  })
+  colnames(res) <- paste0('r', 1:k)
+  
+  return(list(hyp_val = hyp_val, res = res, k = k, constraint = constraint))
+}
+
+###### Calculates allsig -------------------------------------------------------
+# Do not know how this function precisely works.... Copied it from the simulation script.
 allsig <- function(res, hyp_val){
   classic_allsig <- all(apply(res, 2, function(x){
     (x[1]-hyp_val)/x[2] > 1.644854
@@ -8,17 +38,19 @@ allsig <- function(res, hyp_val){
 }
 
 
-tbf <- function(res, hyp_val, n, k){
+###### Calculates tbf_iu and tbf_ic --------------------------------------------
+tbf <- function(res, hyp_val, n, k, constraint){
   bf_together <- bain(res[1,], 
-                      hypothesis = gsub("(r1)", "r1", paste0("(", paste0(colnames(res), collapse = ", "), ") > ", hyp_val), fixed = TRUE), 
+                      hypothesis = gsub("(r1)", "r1", paste0("(", paste0(colnames(res), collapse = ", "), ")", constraint, hyp_val), fixed = TRUE), 
                       n = sum(rep(n, k)),
                       Sigma = diag(res[2,], ncol = ncol(res)))
   return(c(tbf_ic = bf_together$fit$BF.c[1], tbf_iu = bf_together$fit$BF.u[1]))
 }
 
-prod_and_gpbf <- function(res, hyp_val, n, k){
+###### Calculates prodbf_iu, prodbf_ic, gpbf_iu and gpbf_ic --------------------
+prod_and_gpbf <- function(res, hyp_val, n, k, constraint){
   sig <- lapply(res[2,], matrix)
-  bf_individual <- lapply(paste0(colnames(res), ">", hyp_val), 
+  bf_individual <- lapply(paste0(colnames(res), constraint, hyp_val), 
                           bain,       
                           x = res[1,],
                           Sigma = sig,        
@@ -38,3 +70,18 @@ prod_and_gpbf <- function(res, hyp_val, n, k){
   return(c(gpbf_ic = gp_and_prod[1,][1], gpbf_iu = gp_and_prod[1,][2],
            prodbf_ic = gp_and_prod[2,][1], prodbf_iu = gp_and_prod[2,][2]))
 }
+
+##### To generate an example data.frame ----------------------------------------
+example_df <- function(){
+  set.seed(6164900)
+  y <- rnorm(1000)
+  x <- y + rnorm(1000,0,3)
+  z <- rnorm(1000)
+  M <- as.data.frame(cbind(y = y,        # generate outcome variable from std normal dist
+             x = x,                      # generate predictor from normal dist
+             z = z,
+             k = rep(c("A", "B", "C", "D"), each = 250)))   # make 4 groups
+  M[,c(1:3)] <- sapply(M[,c(1:3)], as.numeric)
+  return(M)
+}
+ 
