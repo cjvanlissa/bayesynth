7200e885b97970e911f4150f9f77a255916a6c45
cjvanlissa <c.j.vanlissa@uu.nl>
2022-05-09 19:02:38 +0200

    update 2022-05-09 19:02:38

--- Analysis/analysis.R
+++ Analysis/analysis.R
@@ -1,5 +1,6 @@
 #dat <- readRDS("tabres.RData")
-dat <- readRDS(file.path("Sim_Eli", "sim_results_2022-03-19.RData"))
+library(data.table)
+dat <- readRDS(file.path("Sim", "sim_results_2022-05-08.RData"))
 lapply_at <- function(var, truees) {
   results <- sapply(var, function(var) {
     table(ordered(truees > .1, levels = c("FALSE", "TRUE")), ordered(var > 3, levels = c("FALSE", "TRUE")))
@@ -9,8 +10,8 @@ lapply_at <- function(var, truees) {
                            USE.NAMES = FALSE)
   as.list(results)
 }
-varsout <- c("allsig", "gpbf_ic", "gpbf_iu", "prodbf_ic", "prodbf_iu", "tbf_ic", "tbf_iu")
-varspred <- c("es", "errorsd", "n", "k", "hyp_val")
+varsout <- c("ipd", "rma", "allsig", "gpbf_ic", "gpbf_iu", "prodbf_ic", "prodbf_iu", "tbf_ic", "tbf_iu")
+varspred <- c("errorsd", "n", "k", "hyp_val")
 
 # Example: per condition
 dat[!es < .1, lapply_at(.SD, truees = es), .SDcols = varsout, by = c("n", "errorsd")]
@@ -22,27 +23,103 @@ res <- dat[!es < .1, sapply(.SD, function(var) {
 }), .SDcols = varsout]
 rownames(res) <- c("TN", "FN", "FP", "TP")
 
+tabres <- dat[!es < .1,  lapply(.SD, function(var) {
+  as.vector(table(ordered(es > .1, levels = c("FALSE", "TRUE")), ordered(var > 3, levels = c("FALSE", "TRUE"))))
+}), .SDcols = varsout, by = varspred]
+tabres[, metric := rep(c("TN", "FN", "FP", "TP"), nrow(tabres)/4)]
+
+tabres <- melt(tabres, measure.vars = varsout,
+             variable.name = "alg", value.name = "val")
+tabres <- dcast(tabres, errorsd + n + k + hyp_val + alg ~ metric, value.var = "val")
+tabres[, alpha := FP / (FP + TN)]
+tabres[, beta := FN / (TP + FN)]
+tabres[, sensitivity := 1-beta]
+tabres[, specificity := 1-alpha]
+tabres[, accuracy := (TP + TN) / (TP+TN+FP+FN)]
+
+saveRDS(tabres, "confusion_by_cond.RData")
+
+
+# ANOVAs ------------------------------------------------------------------
+source("analysis_functions.R")
+df_acc <- dcast(tabres, errorsd + n + k + hyp_val ~ alg, value.var = "accuracy")
+#the dependent variables (the test r2)
+yvars <- varsout[!endsWith(varsout, "iu")]
+conditions <- c("n", "k", "errorsd")
+#creates a list for every Anova with the results for the anova and the effect sizes for the conditions on the algorithms
+anovas<-lapply(yvars, function(yvar){
+  form<-paste(yvar, '~', paste(conditions, collapse = "+"))
+  # form<-paste(yvar, '~(', paste(unlist(conditions[-lc]), "+", collapse = ' '), conditions[lc], ") ^ 2") #the ^2 signifies that we want all possible effects up until interactions
+  thisaov<-aov(as.formula(form), data=df_acc) #change data according to dataframe you are using
+  thisetasq<-EtaSq(thisaov)[ , 2]
+  list(thisaov, thisetasq)
+})
+
+# Anova for the difference ------------------------------------------------
+
+comps <- expand.grid(yvars, yvars)
+comps <- comps[!comps$Var1 == comps$Var2, ]
+comps <- t(apply(comps, 1, sort))
+comps <- comps[!duplicated(comps), ]
+#creates a list for every Anova with the results for the anova and the effect sizes for the conditions on the algorithms
+diffanovas <- sapply(1:nrow(comps), function(i){
+  form<-as.formula(paste("r2", '~ algo * (', paste(conditions, collapse = "+"), ")")) #the ^2 signifies that we want all possible effects up until interactions
+  # form<-as.formula(paste("r2", '~ algo * ((', paste(unlist(conditions[-lc]), "+", collapse = ' '), conditions[lc], ") ^ 2)")) #the ^2 signifies that we want all possible effects up until interactions
+  tmp <- df_acc
+  tmp <- tmp[, .SD, .SDcols = c(names(tmp)[1:3], comps[i, , drop = TRUE])]
+  names(tmp) <- gsub("^(.+?)_r2", "r2_\\1", names(tmp))
+  tmp = melt(tmp, id.vars = names(tmp)[1:3],
+             measure.vars = names(tmp)[4:5],
+             variable.name = "algo",
+             value.name = "r2")
+  thisaov<-aov(form, data=tmp) #change data according to dataframe you are using
+  thisetasq<-EtaSq(thisaov)[ , 2]
+  thisetasq <- thisetasq[startsWith(names(thisetasq), "algo")]
+  thisetasq
+})
+colnames(diffanovas) <- paste0(comps[,1], " vs. ", comps[,2])
+diffanovas <- data.frame(condition = gsub("algo:", "", rownames(diffanovas), fixed = T), diffanovas)
+diffanovas$condition <- trimws(diffanovas$condition)
+out <- list(difference = diffanovas[1, ])
+#creates dataframe with effect sizes for all conditions for all algorithms
+etasqs<-data.frame(sapply(anovas, `[[`, 2))
+colnames(etasqs) <- yvars
+etasqs$condition <- trimws(rownames(etasqs))
+etasqs <- merge(etasqs, diffanovas, by = "condition", all.x = TRUE)
+
+write.csv(etasqs, "effect_of_conditions.csv", row.names = FALSE)
+
+
 stats <- apply(res, 2, function(x){
   attach(as.list(x))
   alpha = FP / (FP + TN) #False positive rate 
   beta = FN / (TP + FN) # False negative rate
   sensitivity = 1-beta
   specificity = 1-alpha
+  accuracy = (TP + TN) / sum(x)
   c(False_positive_rate_alpha = alpha,
     False_negative_rate_beta = beta,
     sensitivity = sensitivity,
     specificity = specificity, 
+    accuracy = accuracy,
     pos_lr = sensitivity / (1 - specificity),
     neg_lr = (1 - sensitivity) / specificity)
 })
+stats <- stats[, order(stats["accuracy",], decreasing = T)]
+write.csv(stats, "confusion.csv")
 
 library(ggplot2)
 df_plot <- data.frame(Method = colnames(stats),
                       Sensitivity = stats["sensitivity", ],
                       Specificity = stats["specificity", ])
-
-ggplot(df_plot, aes(x = Sensitivity, y = Specificity, colour = Method, shape = Method)) +
+df_plot$Type <- "Frequentist"
+df_plot$Type[endsWith(df_plot$Method, "iu")] <- "BF unconstrained"
+df_plot$Type[endsWith(df_plot$Method, "ic")] <- "BF complement"
+df_plot <- df_plot[!endsWith(df_plot$Method, "iu"), ]
+ggplot(df_plot, aes(x = Sensitivity, y = Specificity, colour = Method, fill = Method, shape = Type)) +
   geom_point() +
   scale_x_continuous(limits = c(0,1))+
   scale_y_continuous(limits = c(0,1)) +
+  geom_vline(xintercept = .8, linetype = 2) +
+  geom_hline(yintercept = .95, linetype = 2) +
   theme_bw()
---
+++ Sim/functions.R
@@ -0,0 +1,94 @@
+# 
+# 
+# 
+# BFs <- function(es, n, hyp_val, k, errorsd){
+#   
+#   # create dataframe for each group
+#   dfs <- lapply(1:k, function(i){
+#     df <- rmvnorm(n, sigma = matrix(c(1, es, es, 1), nrow = 2))
+#     df + rnorm(2*n, sd = errorsd)
+#   })
+#   
+#   # obtain estimates for correlations and their standard errors for every dataset
+#   res <- sapply(dfs, function(x){
+#     est <- cor(x)[2,1]                        #estimate for correlation between x and y
+#     se_est <- sqrt((1 - est^2)/(n - 2)) #estimate for standard error = sqrt((1-r^2)/df) with df = N - 2
+#     c(est, se_est)               #return estimate, se and sample size of particular set
+#   })
+#   
+#   # Classic approach
+#   classic_allsig <- all(apply(res, 2, function(x){
+#     (x[1]-hyp_val)/x[2] > 1.644854
+#   }))
+#   
+#   # necessary naming for bain and further preparing
+#   colnames(res) <- paste0('r', 1:k)
+#   sig <- lapply(res[2,], matrix)    # make list of covariance matrices for the datasets, as shown in the Bain vignette
+#   #ngroup <- rep(n, k)       # obtain sample size per group
+#   
+#   #run bf_individual to extract product bf and geometric product bf
+#   bf_individual <- lapply(paste0(colnames(res), ">", hyp_val), # for every group, we hypothesize that r_k > hyp_val
+#                           bain,                 # call bain
+#                           x = res[1,],          # estimates
+#                           Sigma = sig,          # all rho's are assumed to be independent
+#                           n = rep(n, k),           # pass the named vector of sample sizes per group to bain
+#                           group_parameters = 1, # every group k has 1 parameter which is rho_xy
+#                           joint_parameters = 0) # they do not share parameters 
+# 
+#   # extract BF_ic and BF_iu for the parameter of every group
+#   BFs <- t(sapply(bf_individual, function(x){
+#     c(x$fit$BF.c[1], x$fit$BF.u[1])
+#   }))
+# 
+#   # obtain geometric product and regular product
+#   gp_and_prod <- apply(BFs, 2, function(x){
+#     prod_bf <- prod(x)         #obtain product bf
+#     c(prod_bf^(1/k), prod_bf)  #concatenate geometric product and regular product
+#   })
+#   
+#   # create bf_together object to obtain the BFs for the (group1, group2, group3) > hyp_val
+#   bf_together <- bain(res[1,], 
+#                       hypothesis = gsub("(r1)", "r1", paste0("(", paste0(colnames(res), collapse = ", "), ") > ", hyp_val), fixed = TRUE), 
+#                       n = sum(rep(n, k)), # n = sum of sample sizes over all groups.
+#                       Sigma = diag(res[2,], ncol = ncol(res)))      # assume independence between groups
+#   
+#   # returns in order: gpbf_ic, gpbf_iu, prodbf_ic, prodbf_iu, tbf_ic, tbf_iu
+#   return(c(
+#     c(0,10)[classic_allsig+1],
+#     gp_and_prod[1,],
+#     gp_and_prod[2,], 
+#     c(bf_together$fit$BF.c[1], bf_together$fit$BF.u[1])))
+# }
+
+
+
+
+#product BF
+# gPBF <- function(BFs){
+#   N  <- ifelse(is.null(nrow(BFs)), length(BFs), nrow(BFs)) #to distinguish between vectors and matrices
+#   
+#   res <- apply(BFs, 2, function(x){
+#     GP <- prod(x) ^ (1 / N) #Geometric mean
+#     ER <- abs((GP < 1) - sum(x > 1)/N) #Evidence Rate
+#     SR <- ifelse(GP < 1, #Stability Rate
+#                  sum(x < GP) / N, 
+#                  sum(x > GP) / N)
+#     c(GP, ER, SR)
+#   })
+#   
+#   rownames(res) <- c("Geometric Product", "Evidence Rate", "Stability Rate")
+#   out <- list("GPBF" = res, "BFs" = BFs, "N" = N)
+#   class(out) <- "gPBF"
+#   return(out)
+# }
+# 
+# 
+# prop_correct <- function(x, BF_threshold, var_n){
+#   if(length(BF_threshold) == 1 && BF_threshold == 3){
+#     sum(x >= BF_threshold) / (var_n)
+#   } else if(length(BF_threshold) == 1 && BF_threshold == 0.33){
+#     sum(x <= 0.33) / (var_n)
+#   }else{
+#     sum(x > min(BF_threshold) & x < max(BF_threshold)) / (var_n)
+#   }
+# }
