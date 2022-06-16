06e4dc00074cd8a69cd89093b743457cd9ef4ba9
cjvanlissa <c.j.vanlissa@uu.nl>
2022-06-16 08:49:32 +0200

    pbf returns data.frame with class pbf

--- UI Function/pbf.R
+++ UI Function/pbf.R
@@ -31,9 +31,10 @@ pbf <- function(x, ...){
     }
   }
   BFs <- do.call(cbind, lapply(x, function(y){y$fit$BF.c[match(hyps, y$hypotheses)]}))
-  rownames(BFs) <- paste0(sprintf('H%d: ', 1:length(hyps)),hyps) # give names
-  
-  res <- list(BFs = BFs, pbf = apply(BFs, 1, prod)) # obtain pbf ic, might need to change dependent on alternative hyp
+  colnames(BFs) <- paste0("Sample ", 1:ncol(BFs))
+  res <- data.frame(PBF = apply(BFs, 1, prod), BFs)# obtain pbf ic, might need to change dependent on alternative hyp
+  rownames(res) <- paste0(sprintf('H%d: ', 1:length(hyps)),hyps) # give names
+  class(res) <- c("pbf", class(res))
   return(res)
 }
 
@@ -60,7 +61,7 @@ results2 <- bain(anov, "site2=site1=site3=site4=site5; site2=site1=site3>site4=s
 anov <- lm(postnumb~site-1,sesamesim[151:nrow(sesamesim),])
 results3 <- bain(anov, "site2=site1=site3=site4=site5; site2=site1=site3=site4>site5; site2>site5>site1>site3>site4")
 
-pbf(list(results, results2, results3))
+res_base <- pbf(list(results, results2, results3))
 
 anov <- lm(postnumb~site-1,sesamesim[1:75,])
 results <- bain(anov, "site2=site1>site3=site4=site5; site2>site1=site3=site4=site5; site2>site5>site1>site3>site4")
@@ -70,3 +71,15 @@ anov <- lm(postnumb~site-1,sesamesim[151:nrow(sesamesim),])
 results3 <- bain(anov, "site2=site1=site3=site4=site5; site2=site1=site3=site4>site5; site2>site5>site1>site3>site4")
 
 pbf(list(results, results2, results3))
+
+anov <- lm(postnumb~site-1,sesamesim[1:75,])
+results <- bain(anov, "site2=site1=site3=site4=site5; site2>site1=site3=site4=site5; site2>site5>site1>site3>site4")
+anov <- lm(postnumb~site-1,sesamesim[76:150,])
+results2 <- bain(anov, "site2=site1=site3>site4=site5; site2=site1=site3=site4=site5; site2>site5>site1>site3>site4")
+anov <- lm(postnumb~site-1,sesamesim[151:nrow(sesamesim),])
+results3 <- bain(anov, "site2=site1=site3=site4>site5; site2>site5>site1>site3>site4; site2=site1=site3=site4=site5")
+
+res_changeorder <- pbf(list(results, results2, results3))
+
+res_base
+res_changeorder
